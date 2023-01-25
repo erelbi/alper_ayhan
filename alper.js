2ba9737201f6d6e8f9ad5197ea9d3b94c9d3ec7f
Ergün bilsel <bilselergun@gmail.com>
2023-01-26 01:30:49 +0300

    first commit

---
+++ alper.js
@@ -0,0 +1,28 @@
+var startDateTime = new Date(2023,0,26,16,59,59,0); 
+var startStamp = startDateTime.getTime();
+
+var newDate = new Date();
+var newStamp = newDate.getTime();
+
+var timer; 
+var soundtimer;
+
+function updateClock() {
+    newDate = new Date();
+    newStamp = newDate.getTime();
+    var diff = Math.round((newStamp-startStamp)/1000);
+    
+    var d = Math.floor(diff/(24*60*60));
+    diff = diff-(d*24*60*60);
+    var h = Math.floor(diff/(60*60));
+    diff = diff-(h*60*60);
+    var m = Math.floor(diff/(60));
+    diff = diff-(m*60);
+    var s = diff;
+    
+    document.getElementById("time-elapsed").innerHTML = d+" gün, "+h+" saat, "+m+" dakika, "+s+" saniyedir sen yoksun!";
+}
+
+
+
+timer = setInterval(updateClock, 1000);
